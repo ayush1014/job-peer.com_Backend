1901a7178257b7d3792c2304d72d63281a05b11c
ayush1014 <60238194+ayush1014@users.noreply.github.com>
2024-02-03 17:40:36 -0600

    Revert "Merge pull request #3 from ayush1014/revert-2-ayush"
    This reverts commit 35a7147b8071883a12726e7a59f6de7d797e2459, reversing
    changes made to 3e1f2cb2ceb526e7ed5ef46e11a66e7ea366d845.

---
+++ models/LeaderBoard.js
@@ -0,0 +1,25 @@
+const { DataTypes, Sequelize } = require('sequelize');
+const db = require('../db_config/db')
+
+const LeaderBoard = db.define('leaderBoard',{
+    id:{
+        type:DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey:true,
+        allowNull:false
+    },
+
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        references: {
+            model: 'Users', // This is the table name
+            key: 'username',
+        }
+    },
+
+    numberOfJobs: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0,
+    }
+})
