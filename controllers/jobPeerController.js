81a05a6855080d6ff035e09b9978ff3de932307d
ayush1014 <60238194+ayush1014@users.noreply.github.com>
2024-02-03 19:06:34 -0600

    added new serach functionality of peer

--- controllers/jobPeerController.js
+++ controllers/jobPeerController.js
@@ -1,2 +1,30 @@
 const Sequelize  = require('sequelize');
 const Peer =  require('../models/Peer');
+const LeaderBoard = require('../models/LeaderBoard');
+const User = require('../models/User');
+
+exports.searchUser = async(req, res) =>{
+    try{
+        let user = req.params.username;
+        const userExists = await User.findByPk(user);
+        
+        if (!userExists){
+            return res.status(404).json({ error: 'User not Found!!'});
+        }
+
+        //Search functionality
+        let peerName = req.params.peerName;
+        const peerExists = await User.findByPk(peerName);
+        if (!peerExists){
+            return res.status(404).json({error: 'peer not found, recheck peer name'});
+        }
+        const searchPeer = {
+            username: peerExists.username,
+            name: peerExists.name,
+            email: peerExists.email
+        }
+        res.status(200).json(searchPeer);
+    } catch(error){
+        console.error('Searching Peer Error: ', error);
+    }
+}
--- routes/router.js
+++ routes/router.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
-const jobController = require('../controllers/jobsController')
+const jobController = require('../controllers/jobsController');
+const jobPeerController = require('../controllers/jobPeerController');
 
 //Auth Routes
 router.post('/register', authController.register);
@@ -15,4 +16,5 @@ router.put('/editJobs/:id', jobController.editUserJobs);
 router.get('/jobStats/:username', jobController.jobStatsPie);
 router.get('/jobStatsByTimeFrame/:username/:timeFrame/:timeZone', jobController.getJobStatsByTimeFrame);
 router.get('/jobDetail/:username/:id', jobController.showJobDetailsSingle);
+router.get('/searchPeer/:username/:peerName', jobPeerController.searchUser);
 module.exports = router;
