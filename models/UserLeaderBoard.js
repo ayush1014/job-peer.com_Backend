12a18e6a98a1efc43527e2a63a91260ae89dd8b6
ayush1014 <60238194+ayush1014@users.noreply.github.com>
2024-02-05 18:50:41 -0600

    added follow request and confirmed follow request functionality

--- models/UserLeaderBoard.js
+++ models/UserLeaderBoard.js
@@ -1,17 +1,15 @@
-const {DataTypes, Sequelize} = require('sequelize');
-const db = require('../db_config/db');
-const User = require('./User');
-const LeaderBoard = require('./LeaderBoard');
+// const {DataTypes, Sequelize} = require('sequelize');
+// const db = require('../db_config/db');
+// const User = require('./User');
+// const LeaderBoard = require('./LeaderBoard');
+// const Peer = require('./Peer');
 
-User.hasOne(LeaderBoard,{
-    foreignKey: 'username',
-    as: 'numberOfJobs',
-    onDelete: 'CASCADE'
-});
+// User.hasOne(LeaderBoard, { foreignKey: 'username' , onDelete: 'CASCADE'});
+// LeaderBoard.belongsTo(User, { foreignKey: 'username' });
 
-LeaderBoard.belongsTo(User,{
-    foreignKey: 'username',
-    as:'user'
-});
+// User.hasMany(Peer, { foreignKey: 'requestingPeer', as: 'RequestingPeers' });
+// User.hasMany(Peer, { foreignKey: 'requestedPeer', as: 'RequestedPeers' });
+// Peer.belongsTo(User, { foreignKey: 'requestingPeer', as: 'RequestingUser' });
+// Peer.belongsTo(User, { foreignKey: 'requestedPeer', as: 'RequestedUser' });
 
-module.exports = {User, LeaderBoard};
+// module.exports = {User, LeaderBoard, Peer};
