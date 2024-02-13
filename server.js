9bc07327bc718df1d5d8d366298f90f1f106364a
ayush1014 <60238194+ayush1014@users.noreply.github.com>
2024-02-12 23:58:11 -0600

    changed cors

--- server.js
+++ server.js
@@ -96,7 +96,7 @@ const { init } = require('./socket'); // Ensure this path is correct
 // Middleware
 app.use(cors({
     //   origin: 'http://localhost:3000',
-    origin:[ "https://www.job-peer.com/", "https://job-peer-com-frontend.vercel.app"],
+    origin:[ "https://job-peer.com/", "https://job-peer-com-frontend.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"]
--- socket.js
+++ socket.js
@@ -6,7 +6,7 @@ function init(httpServer) {
     io = new Server(httpServer, {
         cors: {
             // origin: "http://localhost:3000", 
-            origin:[ "https://www.job-peer.com/", "https://job-peer-com-frontend.vercel.app"],
+            origin:[ "https://job-peer.com/", "https://job-peer-com-frontend.vercel.app"],
             methods: ["GET", "POST", "PUT", "DELETE"],
             credentials: true
         }
